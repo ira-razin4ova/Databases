b322ea7a20a18311e2cfc9bb2213ab8b54a6aa73
ira-razin4ova <ipopova024@gmail.com>
2026-05-01 13:32:42 +0300

    lessons "Migrations and Indexes"

---
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset razinkova:1
+CREATE INDEX idx_student_last_name ON student (last_name);
+
+--changeset razinkova:2
+CREATE INDEX IF NOT EXISTS idx_student_phone_number ON student (phone_number);
+CREATE INDEX IF NOT EXISTS idx_student_student_tiсket ON student (student_tiсket);
+
+--changeset razinkova:3
+create index idx_faculty_name on faculty (name);
+create index idx_faculty_color on faculty (color);
